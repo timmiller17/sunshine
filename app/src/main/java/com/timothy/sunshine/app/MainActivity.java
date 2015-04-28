ee41ead55fcc330abbb83c9efdb010c50cf17ff4
Tim Miller <boilertim@gmail.com>
2015-04-28 17:16:52 -0500

    Add map intent (udacity branch 3.12)

--- app/src/main/java/com/timothy/sunshine/app/MainActivity.java
+++ app/src/main/java/com/timothy/sunshine/app/MainActivity.java
@@ -1,13 +1,19 @@
 package com.timothy.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,24 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
+            return true;
+        }
+        if (id == R.id.view_preferred_location) {
+
+            SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+            String location = settings.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+
+            Log.v(LOG_TAG, "location to send to ACTION_VIEW intent: " + location);
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:0,0?").buildUpon()
+                            .appendQueryParameter("q", location).build());
+
+            //verify an app exists to handle intent then call startActivity()
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
             return true;
         }
 
