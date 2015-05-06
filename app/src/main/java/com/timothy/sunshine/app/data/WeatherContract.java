31c194cdd673ef34a24cab71ba54b46bcb76f5ac
Tim Miller <boilertim@gmail.com>
2015-05-06 17:07:23 -0500

    Define contracts constants, misc file changes due to Android Studio update to v1.2 (udacity branch 4.03)

--- app/src/main/java/com/timothy/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/timothy/sunshine/app/data/WeatherContract.java
@@ -41,6 +41,15 @@ public class WeatherContract {
     public static final class LocationEntry implements BaseColumns {
         public static final String TABLE_NAME = "location";
 
+        //this will be sent to open map api as the location query
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+        //human readable name - better than displaying zip for users
+        public static final String COLUMN_CITY_NAME = "city_name";
+
+        //lat/long info for pinpointing location when launching maps intent
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+        public static final String COLUMN_COORD_LONG = "coord_long";
     }
 
     /* Inner class that defines the table contents of the weather table */
