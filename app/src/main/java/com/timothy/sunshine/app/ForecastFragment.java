73e46a6acfabf7673cd4c94c5866020d579c39ee
Tim Miller <boilertim@gmail.com>
2015-07-06 17:37:46 -0500

    Add ForecastAdapter (udacity branch 4.18)

--- app/src/main/java/com/timothy/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/timothy/sunshine/app/ForecastFragment.java
@@ -1,9 +1,8 @@
 package com.timothy.sunshine.app;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,18 +10,16 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
+import com.timothy.sunshine.app.data.WeatherContract;
 
 /**
  * Created by w35063 on 3/26/2015.
  */
 public class ForecastFragment extends Fragment {
 
-    private ArrayAdapter<String> mForecastAdapter;
+    private ForecastAdapter mForecastAdapter;
 
     public ForecastFragment() {
     }
@@ -57,8 +54,23 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        mForecastAdapter = new ArrayAdapter(getActivity(), R.layout.list_item_forecast,
-                R.id.list_item_forecast_textview, new ArrayList<String>());
+        String locationSetting = Utility.getPreferredLocation(getActivity());
+
+        // sort order, ascending by date
+        String sortOrder = WeatherContract.WeatherEntry.COLUMN_DATE + " ASC";
+
+        Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
+                locationSetting,System.currentTimeMillis());
+
+        Cursor cur = getActivity().getContentResolver().query(
+                weatherForLocationUri,
+                null,
+                null,
+                null,
+                sortOrder
+        );
+
+        mForecastAdapter = new ForecastAdapter(getActivity(), cur, 0);
 
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
@@ -66,23 +78,23 @@ public class ForecastFragment extends Fragment {
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String forecast = mForecastAdapter.getItem(position);
-                Intent intent = new Intent(getActivity(), DetailActivity.class)
-                        .putExtra(Intent.EXTRA_TEXT, forecast);
-                startActivity(intent);
-            }
-        });
+        // comment this section out in branch 4.18 because it is incompatible; will fix later
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+//                String forecast = mForecastAdapter.getItem(position);
+//                Intent intent = new Intent(getActivity(), DetailActivity.class)
+//                        .putExtra(Intent.EXTRA_TEXT, forecast);
+//                startActivity(intent);
+//            }
+//        });
 
         return rootView;
     }
 
     private void updateWeather() {
-        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        String zip = settings.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
-        new FetchWeatherTask(getActivity(), mForecastAdapter).execute(zip);
+        String location = Utility.getPreferredLocation(getActivity());
+        new FetchWeatherTask(getActivity()).execute(location);
     }
 
     @Override
--- app/src/main/java/com/timothy/sunshine/app/MainActivity.java
+++ app/src/main/java/com/timothy/sunshine/app/MainActivity.java
@@ -1,10 +1,8 @@
 package com.timothy.sunshine.app;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -49,9 +47,7 @@ public class MainActivity extends ActionBarActivity {
         }
         if (id == R.id.view_preferred_location) {
 
-            SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
-            String location = settings.getString(getString(R.string.pref_location_key),
-                    getString(R.string.pref_location_default));
+            String location = Utility.getPreferredLocation(this);
 
             Log.v(LOG_TAG, "location to send to ACTION_VIEW intent: " + location);
 
