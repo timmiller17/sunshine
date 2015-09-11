0e68edc9f267dcb785c50b9b84141f9ba0fb0634
unknown <w35063@w35063-02.dsa.mot-mobility.com>
2015-09-11 16:35:16 -0500

    Bug fix: Had been storing units pref in the wrong spot in SharedPreferences

--- app/src/main/java/com/timothy/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/timothy/sunshine/app/SettingsActivity.java
@@ -28,7 +28,7 @@ public class SettingsActivity extends PreferenceActivity
         // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
         // updated when the preference changes.
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
-        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_unit_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_key)));
         Log.v(LOG_TAG, "SettingsActivity onCreate entered...");
     }
 
